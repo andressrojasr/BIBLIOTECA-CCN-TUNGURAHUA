43379726a51dd7a3696915c17a38cfb598fedbd8
Andres <aramirez7310@uta.edu.ec>
2025-05-17 01:07:57 -0500

    added table in books

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +9,48 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  @ViewChild(IonContent, { static: false }) content!: IonContent;
+
+  showScrollToTop = false;
+
+  onScroll(event: CustomEvent) {
+    const scrollTop = event.detail.scrollTop;
+    this.showScrollToTop = scrollTop > 150; // muestra si se ha scrolleado más de 150px
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop(300);
+  }
+  
+  books = [
+    { id: 1000, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 2000, title: '1984', author: 'George Orwell', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 3000, title: 'Don Quijote', author: 'Miguel de Cervantes', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 400, title: 'El Principito', author: 'Antoine de Saint-Exupéry', ubicacion: 'Estante 2, fila 3', ejemplares: 4},
+    { id: 1000, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 2000, title: '1984', author: 'George Orwell', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 3000, title: 'Don Quijote', author: 'Miguel de Cervantes', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 400, title: 'El Principito', author: 'Antoine de Saint-Exupéry', ubicacion: 'Estante 2, fila 3', ejemplares: 4},
+    { id: 1000, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 2000, title: '1984', author: 'George Orwell', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 3000, title: 'Don Quijote', author: 'Miguel de Cervantes', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 400, title: 'El Principito', author: 'Antoine de Saint-Exupéry', ubicacion: 'Estante 2, fila 3', ejemplares: 4},
+    { id: 1000, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 2000, title: '1984', author: 'George Orwell', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 3000, title: 'Don Quijote', author: 'Miguel de Cervantes', ubicacion: 'Estante 2, fila 3', ejemplares: 4 },
+    { id: 400, title: 'El Principito', author: 'Antoine de Saint-Exupéry', ubicacion: 'Estante 2, fila 3', ejemplares: 4}
+  ];
+
   constructor() {}
+
+  editBook(book: any) {
+    console.log('Edit book:', book);
+    // Implement your edit logic here
+  }
+
+  deleteBook(book: any) {
+    console.log('Delete book:', book);
+    // Implement your delete logic here
+  }
 
 }
